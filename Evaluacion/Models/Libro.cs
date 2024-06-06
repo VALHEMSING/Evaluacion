414b9dd7a1ba273dccd97b4e5821a8893f034642
Your Name <you@example.com>
2024-06-06 17:58:30 -0500

    se sube la carpeta de solucion para la evaluacion

---
+++ Evaluacion/Models/Libro.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Libro
+    {
+		private string _tituloLibro;
+
+		public string TituloLibro
+		{
+			get { return _tituloLibro; }
+			set { _tituloLibro = value; }
+		}
+		private int  _capitulos;
+
+		public int  Capitulos
+		{
+			get { return _capitulos; }
+			set { _capitulos = value; }
+		}
+		private int _numeroPaginas;
+
+		public int NumeroPaginas
+		{
+			get { return _numeroPaginas; }
+			set { _numeroPaginas = value; }
+		}
+
+
+
+	}
+}
---
+++ Evaluacion/Models/Usuario.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Usuario : Persona, IRegistrable, IBuscable
+    {
+		private string  _nombreUsuario;
+        private string _emailUsuario;
+
+        public string EmailUsuario
+        {
+            get { return _emailUsuario; }
+            set { _emailUsuario = value; }
+        }
+
+
+        public string  NombreUsuario
+		{
+			get { return _nombreUsuario; }
+			set { _nombreUsuario = value; }
+		}
+        public Usuario(string _nombre, string _email, string _nombreUsuario, string _emailUsuario)
+        {
+            NombreUsuario = _nombreUsuario;
+        }
+
+        public override string MostrarDatos()
+        {
+            return $"Nombre del usuario: {NombreUsuario}, e e-amail  {EmailUsuario} .";
+        }
+
+        public string Registrar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Eliminar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BuscarPorTitulo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
